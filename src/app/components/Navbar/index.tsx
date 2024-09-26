bb6ff93863575fdad705c9c3ae45c10a35a61db3
RemigiuszM <remigiusz.mikolajewicz@thestory.pl>
2024-09-27 00:41:04 +0200

    add title to nav

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -2,7 +2,8 @@ import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
 
 export const Navbar = () => {
     return (
-        <nav className="bg-slate-500 w-full">
+        <nav className="bg-slate-500 w-full p-6 flex justify-between">
+            <h1 className="text-white text-2xl font-semibold">Dasz Strima</h1>
             <SignedOut>
                 <SignInButton/>
             </SignedOut>
