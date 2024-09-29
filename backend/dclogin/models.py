1389dd6506e9645130fc5b79ce993945ed1110a4
arciol <arciool.13@gmail.com>
2024-09-29 17:02:53 +0200

    modify models of database objects

--- backend/dclogin/models.py
+++ backend/dclogin/models.py
@@ -5,8 +5,12 @@ from django.contrib.auth.models import User
 
 
 class DiscordUser(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="discorduser"
+    )
     discord_username = models.CharField(max_length=255)
+    discord_id = models.CharField(max_length=255, null=True, blank=True)
+    avatar = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.discord_username
